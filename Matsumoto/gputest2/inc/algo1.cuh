842e85ff4feb69f53579039415dd63699ce97e1a
Matsumgen <sonnadann@gmail.com>
2026-03-06 17:14:52 +0900

    update

---
+++ Matsumoto/gputest2/inc/algo1.cuh
@@ -0,0 +1,21 @@
+#ifndef ALGO1_CUH_
+#define ALGO1_CUH_
+
+#include <stdint.h>
+#include <vector>
+
+
+const uint32_t tidPerField_list[] = { 0, 0, 0, 0,13, 0, 54, 0, 139, 0, 284, 0, 505, 0, 818, 0, 1239, 0, 1784, 0, 2469, 0, 3310, 0, 4323 };
+
+namespace bs1{
+  std::vector<uint32_t> algo(uint16_t *start_field, uint32_t fsize, int eval_type = 1);
+};
+namespace bs11{
+  std::vector<uint32_t> algo(uint16_t *start_field, uint32_t fsize);
+};
+namespace bs2{
+  std::vector<uint32_t> algo(uint16_t *start_field, uint32_t fsize);
+};
+
+
+#endif
